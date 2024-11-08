c376c1878721ff68f17bf719ee8eb53c7d2b1e7a
cgymxm <1503686084@qq.com>
2024-11-08 22:21:47 +0800

    增加Hello World

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello1 World");
+        System.out.println("new update");
     }
 }
